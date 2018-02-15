f7c363c3d6d53761af92c2e7ed44b05ae9da065c
J Looney <jlooney2@my.westga.edu>
2018-02-14 22:33:47 -0500

    Created Ground Class, added ground image for the sprite, scaled the image to stretch the screen, and got collision detection to work with the ground and end the game.

---
+++ src/edu/westga/cs3212/app/Ground.java
@@ -0,0 +1,17 @@
+package edu.westga.cs3212.app;
+
+public class Ground extends Sprite {
+	
+	public Ground(int x, int y) {
+        super(x, y);
+
+        initializeGround();
+    }
+
+    private void initializeGround() {
+        
+        loadImage("src/images/Spring_Grass_PNG_Transparent_Clip_Art_Image.png");
+        getImageDimensions();
+    }
+
+}
