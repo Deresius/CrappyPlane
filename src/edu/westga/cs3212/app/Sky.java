0479e38e3155f2bed0be6a3e6900a19d96ea25f1
jloon <jlooney2@my.westga.edu>
2018-02-26 01:26:05 -0500

    Add comments to sky and ground

--- src/edu/westga/cs3212/app/Sky.java
+++ src/edu/westga/cs3212/app/Sky.java
@@ -1,18 +1,31 @@
 package edu.westga.cs3212.app;
 
+/**
+ * The Class Sky.
+ */
 public class Sky extends Sprite{
 
+	/**
+	 * Instantiates a new sky.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 */
 	public Sky(int x, int y) {
 		super(x, y);
 		
 		this.initializeSky();
 	}
 
+	/**
+	 * Initialize sky.
+	 */
 	private void initializeSky() {
 		this.loadImage("src/images/sky.png");
 		this.getImageDimensions();
 	}
 
+	
 	@Override
 	public void move() {}
 
