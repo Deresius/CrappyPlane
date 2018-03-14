6f1588649c5d5cb93a529b657894648b1453771e
Jacob Slattery <timberwolf@bellsouth.net>
2018-03-14 18:25:26 -0400

    Created Ground Contructor tests

---
+++ src/edu/westga/cs3212/app/model/groundtests/TestGroundContructor.java
@@ -0,0 +1,65 @@
+package edu.westga.cs3212.app.model.groundtests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.app.model.Ground;
+
+class TestGroundContructor {
+
+	@Test
+	final void testFailNegativeX() {
+		try {
+			new Ground(-3, 2, 300);
+			fail("Ground was created with a negative X");
+		} catch (IllegalArgumentException iae) {
+		}
+	}
+
+	@Test
+	final void testFailNegativeY() {
+		try {
+			new Ground(3, -2, 300);
+			fail("Ground was created with a negative Y");
+		} catch (IllegalArgumentException iae) {
+		}
+	}
+
+	@Test
+	final void testFailMaxXLessThanX() {
+		try {
+			new Ground(3, 2, 2);
+			fail("Ground was created with a negative Y");
+		} catch (IllegalArgumentException iae) {
+		}
+	}
+
+	@Test
+	final void testPassAllSameNumber() {
+		try {
+			new Ground(3, 3, 3);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	final void testPassAllLargerNumbers() {
+		try {
+			new Ground(300, 400, 350);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	final void testPassAllZero() {
+		try {
+			new Ground(0, 0, 0);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
