920c5e9b049572e00c8cca5abc6319df39e25238
jloon <jlooney2@my.westga.edu>
2018-03-26 19:03:11 -0400

    Fixed indentation in score and scoreboard

--- src/edu/westga/cs3212/app/model/Score.java
+++ src/edu/westga/cs3212/app/model/Score.java
@@ -5,49 +5,48 @@ package edu.westga.cs3212.app.model;
  */
 public class Score {
 
-/** The players name. */
-private String playersName;
+	/** The players name. */
+	private String playersName;
 
-/** The score. */
-private int score;
+	/** The score. */
+	private int score;
 
-/**
- * Instantiates a new high score.
- *
- * @param playersName the players name
- * @param score the score
- */
-public Score(String playersName, int score) {
-	if(playersName == null || playersName == "") {
-		throw new IllegalArgumentException("Must enter a name.");
+	/**
+	 * Instantiates a new high score.
+	 *
+	 * @param playersName
+	 *            the players name
+	 * @param score
+	 *            the score
+	 */
+	public Score(String playersName, int score) {
+		if (playersName == null || playersName == "") {
+			throw new IllegalArgumentException("Must enter a name.");
+		}
+		if (score <= 0) {
+			throw new IllegalArgumentException("Must have a positive score.");
+		}
+
+		this.playersName = playersName;
+		this.score = score;
 	}
-	if (score <= 0) {
-		throw new IllegalArgumentException("Must have a positive score.");
+
+	/**
+	 * Gets the players name.
+	 *
+	 * @return the players name
+	 */
+	public String getPlayersName() {
+		return playersName;
 	}
-	
-	this.playersName = playersName;
-	this.score = score;
-}
 
-/**
- * Gets the players name.
- *
- * @return the players name
- */
-public String getPlayersName() {
-	return playersName;
-}
-
-/**
- * Gets the score.
- *
- * @return the score
- */
-public int getScore() {
-	return score;
-}
-
-
-
+	/**
+	 * Gets the score.
+	 *
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
 
 }
--- src/edu/westga/cs3212/app/model/Scoreboard.java
+++ src/edu/westga/cs3212/app/model/Scoreboard.java
@@ -2,48 +2,49 @@ package edu.westga.cs3212.app.model;
 
 import java.util.ArrayList;
 
-
 /**
  * The Class HighScoreList.
  */
 public class Scoreboard {
 
-/** The high score collection. */
-private ArrayList<Score> highScoreCollection;
+	/** The high score collection. */
+	private ArrayList<Score> highScoreCollection;
 
-/**
- * Instantiates a new high score list.
- */
-public Scoreboard() {
-	this.highScoreCollection = new ArrayList<Score>();
-}
+	/**
+	 * Instantiates a new high score list.
+	 */
+	public Scoreboard() {
+		this.highScoreCollection = new ArrayList<Score>();
+	}
 
-/**
- * Gets the high scores.
- *
- * @return the high scores
- */
-public ArrayList<Score> getHighScores() {
-	return this.highScoreCollection;
-}
+	/**
+	 * Gets the high scores.
+	 *
+	 * @return the high scores
+	 */
+	public ArrayList<Score> getHighScores() {
+		return this.highScoreCollection;
+	}
 
-/**
- * Adds the new highscore.
- *
- * @param highScore the high score
- * @return true, if successful
- */
-public boolean add(Score highScore) {
-	return this.highScoreCollection.add(highScore);
-}
+	/**
+	 * Adds the new highscore.
+	 *
+	 * @param highScore
+	 *            the high score
+	 * @return true, if successful
+	 */
+	public boolean add(Score highScore) {
+		return this.highScoreCollection.add(highScore);
+	}
 
-/**
- * Removes a highscore.
- *
- * @param highscore the highscore
- * @return true, if successful
- */
-public boolean remove(Score highscore) {
-	return this.highScoreCollection.remove(highscore);
-}
+	/**
+	 * Removes a highscore.
+	 *
+	 * @param highscore
+	 *            the highscore
+	 * @return true, if successful
+	 */
+	public boolean remove(Score highscore) {
+		return this.highScoreCollection.remove(highscore);
+	}
 }
