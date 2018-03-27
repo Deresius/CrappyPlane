7a98314fbadeef2a0065a49a2286fff884c7d759
jloon <jlooney2@my.westga.edu>
2018-03-26 21:32:42 -0400

    Added Tests for score

---
+++ src/edu/westga/cs3212/app/model/tests/TestScoreConstructor.java
@@ -0,0 +1,23 @@
+package edu.westga.cs3212.app.model.tests;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.app.model.Score;
+
+class TestScoreConstructor {
+
+	@Test
+	void testWhenNullIsPassedForPlayerName() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Score(null,9000);
+		}, "Must enter a name.");
+	}
+
+	@Test
+	void testWhenScoreIsValid() {
+		new Score("Username",9000);
+	}
+	
+}
