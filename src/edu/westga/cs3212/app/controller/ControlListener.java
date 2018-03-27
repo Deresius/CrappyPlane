28704d1625f5da88641f1f66fc45eabd3632dbec
William Leatherman <wleathe1@my.westga.edu>
2018-03-27 00:38:51 -0400

    Added comments and formatting to ControlListener.

--- src/edu/westga/cs3212/app/controller/ControlListener.java
+++ src/edu/westga/cs3212/app/controller/ControlListener.java
@@ -5,40 +5,62 @@ import java.awt.event.KeyEvent;
 
 import edu.westga.cs3212.app.model.Level;
 
+/**
+ * Handles all controls for a Level.
+ * 
+ * @author Team 4
+ *
+ */
 public class ControlListener extends KeyAdapter {
-	
+
 	private Level theLevel;
-	
+
+	/**
+	 * Constructs a ControlListener object with the given Level.
+	 * 
+	 * @param myLevel
+	 *            The Level the ControlListener will handle keys for.
+	 */
 	public ControlListener(Level myLevel) {
 		this.theLevel = myLevel;
-		
+
 	}
 
+	/**
+	 * Handles a key being released.
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 		this.theLevel.getPlane().keyReleased(e);
-		
+
 	}
 
+	/**
+	 * Handles a key being pressed down.
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		this.theLevel.getPlane().keyPressed(e);
 		this.theLevel.setStarted(true);
 		int key = e.getKeyCode();
-		
+
 		if (key == KeyEvent.VK_R) {
 			this.theLevel.setStarted(false);
 			this.theLevel.setInGame(true);
 			this.theLevel.initLevel();
 		}
-		
+
 		if (key == KeyEvent.VK_E) {
 			this.theLevel.setEasyDraw(true);
 		}
 
 	}
-	
-	
+
+	/**
+	 * Returns the Level.
+	 * 
+	 * @return The Level.
+	 */
 	public Level getLevel() {
 		return this.theLevel;
 	}
