419ad0f1f25329c5314e05d939abeae254b2af42
jloon <jlooney2@my.westga.edu>
2018-03-27 00:59:28 -0400

    Added tests level

--- src/edu/westga/cs3212/app/model/tests/TestLevelGetters.java
+++ src/edu/westga/cs3212/app/model/tests/TestLevelGetters.java
@@ -11,6 +11,7 @@ import javax.swing.ImageIcon;
 import org.junit.jupiter.api.Test;
 
 import edu.westga.cs3212.app.model.Level;
+import edu.westga.cs3212.app.model.Plane;
 
 class TestLevelGetters {
 
@@ -38,4 +39,52 @@ class TestLevelGetters {
 		assertEquals(expect, level.getImage());
 	}
 
+	@Test
+	final void testSetStartedTrue() {
+		Level level = new Level();
+		level.setStarted(true);
+		assertEquals(true, level.isStarted());
+	}
+	
+	@Test
+	final void testSetStartedFalse() {
+		Level level = new Level();
+		level.setStarted(false);
+		assertEquals(false, level.isStarted());
+	}
+	
+	@Test
+	final void testSetInGameTrue() {
+		Level level = new Level();
+		level.setInGame(true);
+		assertEquals(true, level.isIngame());
+	}
+	
+	@Test
+	final void testSetInGameFalse() {
+		Level level = new Level();
+		level.setInGame(false);
+		assertEquals(false, level.isIngame());
+	}
+	
+	@Test
+	final void testGetPlane() {
+		Level level = new Level();
+		level.initLevel();
+		assertEquals(true, level.getPlane() != null);
+	}
+	
+	@Test
+	final void testSetEasyDrawTrue() {
+		Level level = new Level();
+		level.setEasyDraw(true);
+		assertEquals(true, level.getEasyDraw());
+	}
+	
+	@Test
+	final void testSetEasyDrawFalse() {
+		Level level = new Level();
+		level.setEasyDraw(false);
+		assertEquals(false, level.getEasyDraw());
+	}
 }
